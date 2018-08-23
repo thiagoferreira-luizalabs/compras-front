97e481deff631bd6f7c61152479b4aa469ccce2b
tferreiraw <thiago.ferreira@luizalabs.com>
2018-08-23 17:25:50 -0300

    React router instalado

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Login />
+        {this.props.children}
         <p>Version {version}</p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import "./index.css"
+import Routes from "./routes/routes"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Routes />, document.getElementById('root'));
 registerServiceWorker();
