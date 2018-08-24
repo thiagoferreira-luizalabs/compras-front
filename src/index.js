19dbc19c5c502cad4e07e63c1c3526f584bc96c3
Eduardo Hattori <hattori@hattori-mac-2.local>
2018-08-24 14:24:37 -0300

    commit initial

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'semantic-ui-css/semantic.min.css';
+import './semantic/dist/semantic.min.css';
 import "./index.css"
 
 ReactDOM.render(<App />, document.getElementById('root'));
