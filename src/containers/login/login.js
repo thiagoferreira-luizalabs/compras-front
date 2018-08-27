12ee5372f6427417d3720b103587094148c070bf
Eduardo Hattori <hattori@hattori-mac-2.local>
2018-08-27 10:54:13 -0300

    adicionando a pasta dist

--- src/containers/login/login.js
+++ src/containers/login/login.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import { Menu, Button } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 
 export default () => (
   <div className="">
-    <Menu>
-      <Menu.Item header>Compras</Menu.Item>
-      <Menu.Item name='aboutUs' />
-      <Menu.Item name='jobs' />
-      <Menu.Item name='locations' />
-      <Menu.Item>
-        <Button primary>Sign up</Button>
-      </Menu.Item>
-
-      <Menu.Item>
-        <Button>Log-in</Button>
-      </Menu.Item>
-    </Menu>
+    <Button primary>MUDAAA</Button>
   </div>
 )
 
