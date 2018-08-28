c2cec0ccfdd47a3b10268449b489cb55aeb5ed4e
Eduardo Hattori <hattori@hattori-mac-2.local>
2018-08-28 11:47:29 -0300

    criação do brand

--- src/containers/login/Login.js
+++ src/containers/login/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid, Image, Button, Checkbox, Form } from 'semantic-ui-react'
 import Lu from '../../assets/img/lu.png';
 import Logo from '../../assets/img/magazineluiza.png';
 
@@ -8,15 +8,26 @@ const Login = ({ setAuth }) => {
     <div className="login">
       <div className="box">
         <Grid>
-          <Grid.Column width={6} className="brand">
+          <Grid.Column width={6} className="brand" only='computer'>
               <Image src={Logo} className="logo"  />
               <span>
                   Acesse sistemas, e administrativos tudo em um só lugar.
               </span>
               <Image src={Lu} className="lu" />
           </Grid.Column>
-          <Grid.Column width={10} className="form">
-            b
+          <Grid.Column computer={10} mobile={16} className="form">
+            <Form>
+                <h1>Acesso ao Sistema</h1>
+                <Form.Field>
+                  <label>Usuário</label>
+                  <input placeholder='Digite seu usuário' />
+                </Form.Field>
+                <Form.Field>
+                  <label>Senha</label>
+                  <input placeholder='Digite sua senha' />
+                </Form.Field>                
+                <Button type='submit'>Acessar</Button>                
+            </Form>
           </Grid.Column>
         </Grid>
       </div>
