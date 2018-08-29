e156af1fb603a343dff08b13555716ebdaead43f
tferreiraw <thiago.ferreira@luizalabs.com>
2018-08-29 10:16:08 -0300

    Strips menu

--- src/app/components/page-header/PageHeader.js
+++ src/app/components/page-header/PageHeader.js
@@ -2,22 +2,44 @@ import React from 'react';
 import { Menu, Button, Dropdown } from 'semantic-ui-react'
 import { Link } from "react-router-dom"
 
+const Strips = () => (
+  <div>
+    <div className="strips">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div className="clear"></div>
+  </div>
+)
+
+
 export default () => (
-  <Menu className="navbar">
-    <Link className="item active" to="/home">Compras</Link>
-    <Link className="item" to="/theme">Theme</Link>
-    <Menu.Item>
-      Teste
+  <div>
+    <Strips />
+    <Menu className="navbar">
+      <Link className="item active" to="/home">Compras</Link>
+      <Link className="item" to="/theme">Theme</Link>
+      <Menu.Item>
+        Teste
     </Menu.Item>
-    <Dropdown item text='Categories'>
-      <Dropdown.Menu>
-        <Dropdown.Item>Electronics</Dropdown.Item>
-        <Dropdown.Item>Automotive</Dropdown.Item>
-        <Dropdown.Item>Home</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-    <Menu.Item>
-      <Link to="/logout"><Button>Logout</Button></Link>
-    </Menu.Item>
-  </Menu>
+      <Dropdown item text='Categories'>
+        <Dropdown.Menu>
+          <Dropdown.Item>Electronics</Dropdown.Item>
+          <Dropdown.Item>Automotive</Dropdown.Item>
+          <Dropdown.Item>Home</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <Menu.Item>
+        <Link to="/logout"><Button>Logout</Button></Link>
+      </Menu.Item>
+    </Menu>
+  </div>
 )
